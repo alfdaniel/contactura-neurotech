b9df172e9234c82c963d95c29dc694922f85988b
alfdaniel <daniel.alexandre22@hotmail.com>
2022-01-15 22:24:57 -0300

    atualizando config security

--- backend/src/main/java/com/contactura/contactura/config/SecurityConfig.java
+++ backend/src/main/java/com/contactura/contactura/config/SecurityConfig.java
@@ -5,20 +5,25 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.contactura.contactura.service.CustomUserDetailService;
+
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//	@Autowired
-//	private CustomUserDetailService customUserDetailService;
+	@Autowired
+	private CustomUserDetailService customUserDetailService;
 //
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
@@ -43,10 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //	//
 //	// }
 //
-//	@Autowired
-//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.userDetailsService(customUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
-//	}
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 
 	@Autowired
 	private Environment env;
